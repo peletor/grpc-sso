44048406f0d0081ffe34920748e79d5750a0e4b1
peletor <peletor@yandex.ru>
2024-08-28 13:31:25 +0300

    Change proto Makefile

--- protos/gen/go/sso/sso_grpc.pb.go
+++ protos/gen/go/sso/sso_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.27.3
-// source: proto/sso/sso.proto
+// source: sso/sso.proto
 
 package sso
 
@@ -193,5 +193,5 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/sso/sso.proto",
+	Metadata: "sso/sso.proto",
 }
